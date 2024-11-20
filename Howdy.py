17755985aef30bfd2ca52294c3d507da46f4872b
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-11-20 17:47:50 +0530

    Add files via upload

---
+++ Howdy.py
@@ -0,0 +1,9 @@
+#THIS PARTICULAR PROGRAM PRINTS DIFFERENT TYPES OF HELLO
+#The OBJECTIVE OF THIS CODE IS TO UNDERSTAND THE IF ELSE CONDITIONS
+number = int(input('Enter a number'))
+if(number == 1):
+    print('Hello!')
+elif(number == 2):
+    print('Howdy!')
+else:
+    print('Greetings')
