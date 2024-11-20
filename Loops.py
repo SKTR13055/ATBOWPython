ef17b33822f1002c63732f4ed4c4e0af78ba396b
Syed Khaild Tipu Razvi <110707546+SKTR13055@users.noreply.github.com>
2024-11-20 17:48:03 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,12 @@
+#AIM OF THE PROGRAM IS TO UNDERSTAND HOW THE LOOPS WORK
+#WRITE A SHORT PROGRAM THAT PRINTS THE NUMBERS FROM 1 TO 10 USING A FOR LOOP THEN WRITE THE EQUIVALENT PROGRAM THAT PRINTS THE NUMBERS USING A 
+#WHILE LOOP
+
+for number in range(1,11): #The first argument is where the number will start(1) and second argument is where it will end(11){which is till 10}
+    print(number)
+    
+#Now printing using the while loop
+i = 1 
+while i<11:
+    print(i)
+    i = i+1
